15dce1b30a163260c8a1577631895d101f164409
Alexey Ozeritsky <aozeritsky@gmail.com>
2009-09-16 01:14:30 +0400

    fix

--- to_psql.pl
+++ to_psql.pl
@@ -1,9 +1,13 @@
 #!/usr/bin/perl
-use DBI
+
+use strict;
+
+use DBI;
+use Digest::MD5  qw(md5 md5_hex md5_base64);
 
 print "drivers:\n";
-@drivers = DBI->available_drivers;
-for $i (@drivers) {
+my @drivers = DBI->available_drivers;
+for my $i (@drivers) {
 	print "$i\n";
 }
 print "---\n";
@@ -12,7 +16,7 @@ my $db_str   = "dbi:Pg:dbname=science";
 my $username = "postgres";
 my $password = "";
 
-my $dbh = DBI->connect($db_str, $username, $password, {PrintError => 0});
+my $dbh = DBI->connect($db_str, $username, $password, {PrintError => 0, AutoCommit => 0});
 if ($DBI::err != 0) {
 	print $DBI::errstr . "\n";
 	exit($DBI::err);
@@ -21,7 +25,8 @@ if ($DBI::err != 0) {
 my $id = "CREATE SEQUENCE experiment_id_seq";
 my $table1 = "CREATE TABLE experiment (
 	id INT UNIQUE,
-	name VARCHAR(256) -- link to experiment_table
+	name VARCHAR(256), -- link to experiment_table
+	t timestamp
 )";
 my $alter = "ALTER TABLE experiment ALTER COLUMN id SET DEFAULT NEXTVAL('experiment_id_seq')";
 
@@ -36,29 +41,93 @@ my $table2 = "CREATE TABLE barvortex_fdm (
 	k1 FLOAT8,
 	k2 FLOAT8,
 	theta FLOAT8,
-	rp_info TEXT,
-	coriolis_info TEXT,
-	initial_info TEXT,
-	build_info TEXT,
-	other_info TEXT,
-	cmd_info TEXT, -- command line
+	rp TEXT,
+	coriolis TEXT,
+	initial TEXT,
+	build TEXT,
+	other TEXT,
+	cmd TEXT, -- command line
 	calc_table VARCHAR(256)
 )";
+my $table2_index = "CREATE INDEX calc_table_idx ON barvortex_fdm(calc_table)";
 
 $dbh->do($id);
 $dbh->do($table1);
 $dbh->do($alter);
 $dbh->do($table2);
+$dbh->do($table2_index);
+$dbh->commit();
 
-my $uniq_name = $ARGV[1]; #md5 of input data
-my $table3 = "CREATE TABLE barvortex_fdm_$uniq_name (
-	t FLOAT8,
-	v TEXT
-)";
-$dbh->do($table3);
+sub create_uniq_name($) {
+	my ($ins) = @_;
+	my $hash = md5_hex $ins;
+	print "hash => $hash\n";
+	return $hash;
+}
 
+sub create_calc_table($) {
+	my ($ins) = @_;
+	my $uniq_name = create_uniq_name($ins); #md5 of input data
+	my $uniq_table_name = "barvortex_fdm_$uniq_name";
 
+	my $ans = $dbh->selectall_arrayref("SELECT * FROM barvortex_fdm WHERE calc_table=?",undef,$uniq_table_name);
+	my $upd = 0;
+	if (scalar @$ans) {
+		print STDERR "updating experiment!\n";
+		$upd = 1;
+	} else {
+		print STDERR "create new experiment!\n";
+	}
 
+	$dbh->begin_work();
+	$dbh->do("DROP TABLE IF EXISTS $uniq_table_name");
+	my $table3 = "CREATE TABLE $uniq_table_name (
+		t FLOAT8,
+		v TEXT
+	)";
+	$dbh->do($table3);
+	if (not $upd) {
+		# todo > fill experiment
+		$dbh->do($ins);
+	}
+	$dbh->commit();
+	return $upd;
+}
 
+sub create_insert_string($)
+{
+	my ($h) = @_;
+	my $s = "INSERT INTO barvortex_fdm (";
+	my $fst = 1;
+	while (my ($key, $value) = each(%$h)) {
+		$s .= "$key,";
+	}
+	chop $s;
+	$s .= ") VALUES(";
+	while (my ($key, $value) = each(%$h)) {
+		$s .= "'$value',";
+	}
+	chop $s;
+	$s .= ")";
+	print "=> $s\n";
+}
 
+my %fields = ();
+open(PIPE, "./t.sh 2>&1 | ");
+
+my $read_data = 0;
+while(<PIPE>) {
+	if (not $read_data) {
+		if ($_ =~ m/^#([^:]+):(.*)/) {
+			$fields{$1}=$2;
+		} else {
+			# data begins
+			$read_data = 1;
+			create_calc_table(create_insert_string(\%fields));
+		}
+	}
+
+	if ($read_data) {
+	}
+}
 
