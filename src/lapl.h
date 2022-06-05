7ef188af88a3b614c29c62a5c03c06c4d619486d
Alexey Ozeritskiy <aozeritsky@gmail.com>
2022-06-05 12:11:44 +0300

    Add simple lapl solver and test

--- src/lapl.h
+++ src/lapl.h
@@ -2,12 +2,15 @@
 
 #include "tensor.h"
 #include "asp_fft.h"
+#include "asp_misc.h"
 #include "verify.h"
 
 namespace fdm {
 
 template<typename T, template<typename> class Solver, bool check>
 class LaplCyl3 {
+public:
+
     const double R, r0;
     const double h1, h2;
 
@@ -18,7 +21,6 @@ class LaplCyl3 {
     // strange
     std::vector<Solver<T>> solver;
 
-public:
     LaplCyl3(double R, double r0, double h1, double h2,
              int nr, int nz, int nphi)
         : R(R), r0(r0)
@@ -31,7 +33,9 @@ public:
         init_solver();
     }
 
-    void solve(double* ans, const double* rhs) {
+    void solve(T* ans, T* rhs) {
+        constexpr T SQRT_M_1_PI = 0.56418958354775629;
+
         using tensor_flags = fdm::tensor_flags<tensor_flag::periodic>;
 
         std::vector<int> indices = {0, nphi-1, 1, nz, 1, nr};
@@ -51,7 +55,7 @@ public:
                     s[i] = RHS[i][k][j];
                 }
 
-                pFFT_2(&S[0], &s[0], dphi /* check */, ft);
+                pFFT_2_1(&S[0], &s[0], dphi*SQRT_M_1_PI /* check */, ft);
 
                 for (int i = 0; i < nphi; i++) {
                     RHSm[i][k][j] = S[i];
@@ -60,8 +64,7 @@ public:
         }
 
         for (int i = 0; i < nphi; i++) {
-            // решаем i систем
-            // TODO: init solver
+            // решаем nphi систем
             solver[i].solve(&X[1][1], &RHSm[i][1][1]);
 
             RHSm[i].assign(X.acc);
@@ -73,7 +76,7 @@ public:
                     s[i] = RHSm[i][k][j];
                 }
 
-                pFFT_2_1(&S[0], &s[0], dphi /* check*/, ft);
+                pFFT_2(&S[0], &s[0], SQRT_M_1_PI /* check*/, ft);
 
                 for (int i = 0; i < nphi; i++) {
                     ANS[i][k][j] = S[i];
@@ -95,7 +98,7 @@ private:
         tensor<T,2,check> RHS_phi({1,nz,1,nr});
         csr_matrix<T> P_phi;
 
-        T lm = 1.0; // TODO: eigenvalue
+        T lm = 4.0/dphi2*asp::sq(sin(i*dphi*0.5));
 
         for (int k = 1; k <= nz; k++) {
             for (int j = 1; j <= nr; j++) {
@@ -110,7 +113,7 @@ private:
                     P_phi.add(id, RHS_phi.index({k,j-1}), (r-0.5*dr)/dr2/r);
                 }
 
-                P_phi.add(id, RHS_phi.index({k,j}), -2/dr2-2/dz2+lm/r/r);
+                P_phi.add(id, RHS_phi.index({k,j}), -2/dr2-2/dz2-lm/r/r);
 
                 if (j < nr) {
                     P_phi.add(id, RHS_phi.index({k,j+1}), (r+0.5*dr)/dr2/r);
@@ -125,6 +128,98 @@ private:
         P_phi.sort_rows();
 
         solver[i] = std::move(P_phi);
+    }
+};
+
+
+template<typename T, template<typename> class Solver, bool check>
+class LaplCyl3Simple {
+public:
+
+    const double R, r0;
+    const double h1, h2;
+
+    const int nr, nz, nphi;
+    const double dr, dz, dphi;
+    const double dr2, dz2, dphi2;
+
+    Solver<T> solver;
+
+    LaplCyl3Simple(double R, double r0, double h1, double h2,
+             int nr, int nz, int nphi)
+        : R(R), r0(r0)
+        , h1(h1), h2(h2)
+        , nr(nr), nz(nz), nphi(nphi)
+        , dr((R-r0)/nr), dz((h2-h1)/nz), dphi(2*M_PI/nphi)
+        , dr2(dr*dr), dz2(dz*dz), dphi2(dphi*dphi)
+    {
+        init_solver();
+    }
+
+    void solve(T* ans, T* rhs) {
+        using tensor_flags = fdm::tensor_flags<tensor_flag::periodic>;
+        std::vector<int> indices = {0, nphi-1, 1, nz, 1, nr};
+
+        tensor<T,3,check,tensor_flags> RHS(indices, rhs);
+        tensor<T,3,check,tensor_flags> ANS(indices, ans);
+
+        solver.solve(&ANS[0][1][1], &RHS[0][1][1]);
+    }
+
+private:
+    void init_solver() {
+        using tensor_flags = fdm::tensor_flags<tensor_flag::periodic>;
+
+        std::vector<int> indices = {0, nphi-1, 1, nz, 1, nr};
+        tensor<T,3,check,tensor_flags> RHS(indices);
+
+        csr_matrix<T> P;
+
+        for (int i = 0; i < nphi; i++) {
+            for (int k = 1; k <= nz; k++) {
+                for (int j = 1; j <= nr; j++) {
+                    int id = RHS.index({i,k,j});
+                    double r = r0+j*dr-dr/2;
+                    double r2 = r*r;
+                    //double rm1 = (r-0.5*dr)/r;
+                    //double rm2 = (r+0.5*dr)/r;
+                    double rm = 0;
+                    double zm = 0;
+
+                    /*if (j > 1)  { rm += rm1; }
+                    if (j < nr) { rm += rm2; }
+                    if (k > 1)  { zm += 1; }
+                    if (k < nz) { zm += 1; }*/
+                    zm = 2; rm = 2;
+
+                    P.add(id, RHS.index({i-1,k,j}), 1/dphi2/r2);
+
+                    if (k > 1) {
+                        P.add(id, RHS.index({i,k-1,j}), 1/dz2);
+                    }
+
+                    if (j > 1) {
+                        P.add(id, RHS.index({i,k,j-1}), (r-0.5*dr)/dr2/r);
+                    }
+
+                    P.add(id, RHS.index({i,k,j}), -rm/dr2-zm/dz2-2/dphi2/r2);
+
+                    if (j < nr) {
+                        P.add(id, RHS.index({i,k,j+1}), (r+0.5*dr)/dr2/r);
+                    }
+
+                    if (k < nz) {
+                        P.add(id, RHS.index({i,k+1,j}), 1/dz2);
+                    }
+
+                    P.add(id, RHS.index({i+1,k,j}), 1/dphi2/r2);
+                }
+            }
+        }
+        P.close();
+        P.sort_rows();
+
+        solver = std::move(P);
     }
 };
 
--- ut/ut_lapl.cpp
+++ ut/ut_lapl.cpp
@@ -7,6 +7,7 @@
 
 #include "umfpack_solver.h"
 #include "lapl.h"
+#include "config.h"
 
 extern "C" {
 #include <cmocka.h>
@@ -14,17 +15,186 @@ extern "C" {
 
 using namespace fdm;
 using namespace std;
+using namespace asp;
 
-void test_lapl_cyl(void** ) {
-    LaplCyl3<double, umfpack_solver, true> lapl(
-        M_PI, M_PI/2, 0, 10,
-        32, 32, 32
-        );
+//(z-h0)*(z-h1)*(r-r0)*(r-R)*(sin(φ) + cos(φ))
+
+double ans(int i, int k, int j, double dr, double dz, double dphi, double r0, double R, double h0, double h1) {
+    double r = r0+dr*j-dr/2;
+    double z = h0+dz*k-dz/2;
+    double φ = dphi*(i+1)-dphi/2;
+    double f = (z-h0)*(z-h1)*(r-r0)*(r-R)*(sin(φ) + cos(φ));
+    return f;
 }
 
-int main() {
+//((r-r0)*(z-h0)*(z-h1)*(sin(φ)+cos(φ))+(r-R)*(z-h0)*(z-h1)*(sin(φ)+cos(φ))+2*r*(z-h0)*(z-h1)*(sin(φ)+cos(φ)))/r+2*(r-R)*(r-r0)*(sin(φ)+cos(φ))+((r-R)*(r-r0)*(z-h0)*(z-h1)*(-sin(φ)-cos(φ)))/r^2
+
+double rp(int i, int k, int j, double dr, double dz, double dphi, double r0, double R, double h0, double h1) {
+    double r = r0+dr*j-dr/2;
+    double z = h0+dz*k-dz/2;
+    double φ = dphi*(i+1)-dphi/2;
+    double f = ((r-r0)*(z-h0)*(z-h1)*(sin(φ)+cos(φ))
+                +(r-R)*(z-h0)*(z-h1)*(sin(φ)+cos(φ))
+                +2*r*(z-h0)*(z-h1)*(sin(φ)+cos(φ)))/r
+        +2*(r-R)*(r-r0)*(sin(φ)+cos(φ))
+        +((r-R)*(r-r0)*(z-h0)*(z-h1)*(-sin(φ)-cos(φ)))/r/r;
+    return f;
+}
+
+void test_lapl_cyl_simple(void** data) {
+    constexpr bool check = true;
+    using T = double;
+    using tensor_flags = fdm::tensor_flags<tensor_flag::periodic>;
+
+    Config* c = static_cast<Config*>(*data);
+    int nr = c->get("test", "nr", 32);
+    int nz = c->get("test", "nz", 32);
+    int nphi = c->get("test", "nphi", 32);
+    int verbose = c->get("test", "verbose", 0);
+    double r0 = M_PI/2, R = M_PI;
+    double h0 = 0, h1 = 10;
+    LaplCyl3Simple<double, umfpack_solver, true> lapl(
+        R, r0, h0, h1,
+        nr, nz, nphi
+        );
+    double dr, dz, dphi;
+    dr = lapl.dr; dz = lapl.dz; dphi = lapl.dphi;
+
+    std::vector<int> indices = {0, nphi-1, 1, nz, 1, nr};
+    tensor<T,3,check,tensor_flags> RHS(indices);
+    tensor<T,3,check,tensor_flags> ANS(indices);
+
+    for (int i = 0; i < nphi; i++) {
+        for (int k = 1; k <= nz; k++) {
+            for (int j = 1; j <= nr; j++) {
+                double r = r0+dr*j-dr/2;
+
+                RHS[i][k][j] = rp(i, k, j, dr, dz, dphi, r0, R, h0, h1);
+
+                if (k <= 1) {
+                    RHS[i][k][j] -= ans(i,k-1,j,dr,dz,dphi, r0, R, h0, h1)/dz/dz;
+                }
+                if (j <= 1) {
+                    RHS[i][k][j] -= (r-dr/2)/2*ans(i,k,j-1,dr,dz,dphi,r0,R,h0,h1)/dr/dr;
+                }
+                if (j >= nr) {
+                    RHS[i][k][j] -= (r+dr/2)/2*ans(i,k,j+1,dr,dz,dphi,r0,R,h0,h1)/dr/dr;
+                }
+                if (k >= nz) {
+                    RHS[i][k][j] -= ans(i,k+1,j,dr,dz,dphi,r0,R,h0,h1)/dz/dz;
+                }
+            }
+        }
+    }
+
+    lapl.solve(&ANS[0][1][1], &RHS[0][1][1]);
+
+    double nrm = 0.0;
+    double nrm1 = 0.0;
+    for (int i = 0; i < nphi; i++) {
+        for (int k = 1; k <= nz; k++) {
+            for (int j = 1; j <= nr; j++) {
+                double f = ans(i,k,j, dr, dz, dphi,r0,R,h0,h1);
+                if (verbose) {
+                    printf("%e %e %e %e\n",
+                           ANS[i][k][j], f,
+                           ANS[i][k][j]/f,
+                           std::abs(ANS[i][k][j]-f));
+                }
+
+                nrm = std::max(nrm, std::abs(ANS[i][k][j]-f));
+                nrm1 = std::max(nrm1, std::abs(f));
+            }
+        }
+    }
+    nrm /= nrm1;
+    if (verbose) {
+        printf("%e\n", nrm);
+    }
+
+    assert_true(nrm < 5e-2);
+}
+
+void test_lapl_cyl(void** data) {
+    Config* c = static_cast<Config*>(*data);
+    constexpr bool check = true;
+    using T = double;
+    using tensor_flags = fdm::tensor_flags<tensor_flag::periodic>;
+
+    int nr = c->get("test", "nr", 32);
+    int nz = c->get("test", "nz", 32);
+    int nphi = c->get("test", "nphi", 32);
+    int verbose = c->get("test", "verbose", 0);
+    double r0 = M_PI/2, R = M_PI;
+    double h0 = 0, h1 = 10;
+    LaplCyl3<double, umfpack_solver, true> lapl(
+        R, r0, h0, h1,
+        nr, nz, nphi
+        );
+    double dr, dz, dphi;
+    dr = lapl.dr; dz = lapl.dz; dphi = lapl.dphi;
+
+    std::vector<int> indices = {0, nphi-1, 1, nz, 1, nr};
+    tensor<T,3,check,tensor_flags> RHS(indices);
+    tensor<T,3,check,tensor_flags> ANS(indices);
+
+    for (int i = 0; i < nphi; i++) {
+        for (int k = 1; k <= nz; k++) {
+            for (int j = 1; j <= nr; j++) {
+                double r = r0+dr*j-dr/2;
+
+                RHS[i][k][j] = rp(i, k, j, dr, dz, dphi, r0, R, h0, h1);
+
+                if (k <= 1) {
+                    RHS[i][k][j] -= ans(i,k-1,j,dr,dz,dphi, r0, R, h0, h1)/dz/dz;
+                }
+                if (j <= 1) {
+                    RHS[i][k][j] -= (r-dr/2)/2*ans(i,k,j-1,dr,dz,dphi,r0,R,h0,h1)/dr/dr;
+                }
+                if (j >= nr) {
+                    RHS[i][k][j] -= (r+dr/2)/2*ans(i,k,j+1,dr,dz,dphi,r0,R,h0,h1)/dr/dr;
+                }
+                if (k >= nz) {
+                    RHS[i][k][j] -= ans(i,k+1,j,dr,dz,dphi,r0,R,h0,h1)/dz/dz;
+                }
+            }
+        }
+    }
+
+    lapl.solve(&ANS[0][1][1], &RHS[0][1][1]);
+
+    double nrm = 0.0;
+    double nrm1 = 0.0;
+    for (int i = 0; i < nphi; i++) {
+        for (int k = 1; k <= nz; k++) {
+            for (int j = 1; j <= nr; j++) {
+                double f = ans(i,k,j, dr, dz, dphi,r0,R,h0,h1);
+                if (verbose) {
+                    printf("%e %e %e %e\n",
+                           ANS[i][k][j], f,
+                           ANS[i][k][j]/f,
+                           std::abs(ANS[i][k][j]-f));
+                }
+                nrm = std::max(nrm, std::abs(ANS[i][k][j]-f));
+                nrm1 = std::max(nrm1, std::abs(f));
+            }
+        }
+    }
+    nrm /= nrm1;
+    if (verbose) {
+        printf("%e\n", nrm);
+    }
+}
+
+int main(int argc, char** argv) {
+    string config_fn = "ut_lapl.ini";
+    Config c;
+    c.open(config_fn);
+    c.rewrite(argc, argv);
+
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(test_lapl_cyl)
+        cmocka_unit_test_prestate(test_lapl_cyl_simple, &c),
+        cmocka_unit_test_prestate(test_lapl_cyl, &c)
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
