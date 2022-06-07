d7b331de39beb7996c4e73e8b4186a03aab1a069
Alexey Ozeritskiy <aozeritsky@gmail.com>
2022-06-08 02:20:15 +0300

    Add fft-2d

--- src/lapl2d.h
+++ src/lapl2d.h
@@ -9,7 +9,7 @@
 
 namespace fdm {
 
-template<typename T, bool check>
+template<typename T, bool check, bool fft2=false>
 class Lapl2d {
 public:
     using matrix = tensor<T,2,check>;
@@ -17,13 +17,21 @@ public:
     const double dx2, dy2;
 
     const double lx, ly;
+    const double slx, sly;
 
     const int nx, ny;
 
     std::vector<T> L,D,U;
 
-    FFTTable<T> ft_table;
-    FFT<T> ft;
+    FFTTable<T> ft_y_table;
+    FFT<T> ft_y;
+    FFTTable<T>  ft_x_table_;
+    FFTTable<T>* ft_x_table;
+    FFT<T> ft_x;
+
+    std::vector<T> lm_y;
+    std::vector<T> lm_x_;
+    T* lm_x;
 
     /**
        \param dx, dy - расстояние между точками
@@ -36,16 +44,30 @@ public:
         : dx(dx), dy(dy)
         , dx2(dx*dx), dy2(dy*dy)
         , lx(lx), ly(ly)
+        , slx(sqrt(2./lx)), sly(sqrt(2./ly))
         , nx(nx), ny(ny)
         , L(nx-1), D(nx), U(nx-1)
-        , ft_table(ny+1)
-        , ft(ft_table, ny+1)
-    { }
+        , ft_y_table(ny+1)
+        , ft_y(ft_y_table, ny+1)
+        , ft_x_table_(nx == ny ? 1 : ny+1)
+        , ft_x_table(nx == ny ? &ft_y_table: &ft_x_table_)
+        , ft_x(*ft_x_table, nx+1)
+    {
+        init_lm();
+    }
 
     /**
        \param rhs, ans - массивы размера (ny-1)*(nx-1) - только внутренние точки
      */
     void solve(T* ans, T* rhs) {
+        if constexpr(fft2==true) {
+            solve2(ans, rhs);
+        } else {
+            solve1(ans, rhs);
+        }
+    }
+
+    void solve1(T* ans, T* rhs) {
         matrix ANS({1,ny,1,nx}, ans);
         matrix RHS({1,ny,1,nx}, rhs);
         matrix RHSm({1,ny,1,nx});
@@ -56,7 +78,7 @@ public:
                 s[k] = RHS[k][j];
             }
 
-            ft.sFFT(&S[0], &s[0], dy*sqrt(2./ly));
+            ft_y.sFFT(&S[0], &s[0], dy*sqrt(2./ly));
 
             for (int k = 1; k <= ny; k++) {
                 RHSm[k][j] = S[k];
@@ -74,9 +96,70 @@ public:
         for (int j = 1; j <= nx; j++) {
             for (int k = 1; k <= ny; k++) {
                 s[k] = RHSm[k][j];
-
-                ft.sFFT(&S[0], &s[0], sqrt(2./ly));
             }
+
+            ft_y.sFFT(&S[0], &s[0], sqrt(2./ly));
+
+            for (int k = 1; k <= ny; k++) {
+                ANS[k][j] = S[k];
+            }
+        }
+    }
+
+    void solve2(T* ans, T* rhs) {
+        matrix ANS({1,ny,1,nx}, ans);
+        matrix RHS({1,ny,1,nx}, rhs);
+        matrix RHSm({1,ny,1,nx});
+        std::vector<T> s(ny+1), S(ny+1);
+
+        for (int j = 1; j <= nx; j++) {
+            for (int k = 1; k <= ny; k++) {
+                s[k] = RHS[k][j];
+            }
+
+            ft_y.sFFT(&S[0], &s[0], dy*sly);
+
+            for (int k = 1; k <= ny; k++) {
+                RHSm[k][j] = S[k];
+            }
+        }
+
+        for (int k = 1; k <= ny; k++) {
+            for (int j = 1; j <= nx; j++) {
+                s[j] = RHSm[k][j];
+            }
+
+            ft_x.sFFT(&S[0], &s[0], dx*slx);
+
+            for (int j = 1; j <= nx; j++) {
+                RHSm[k][j] = S[j];
+            }
+        }
+
+        for (int k = 1; k <= ny; k++) {
+            for (int j = 1; j <= nx; j++) {
+                RHSm[k][j] /= -lm_y[k]-lm_x[j];
+            }
+        }
+
+        for (int k = 1; k <= ny; k++) {
+            for (int j = 1; j <= nx; j++) {
+                s[j] = RHSm[k][j];
+            }
+
+            ft_x.sFFT(&S[0], &s[0], slx);
+
+            for (int j = 1; j <= nx; j++) {
+                ANS[k][j] = S[j];
+            }
+        }
+
+        for (int j = 1; j <= nx; j++) {
+            for (int k = 1; k <= ny; k++) {
+                s[k] = ANS[k][j];
+            }
+
+            ft_y.sFFT(&S[0], &s[0], sly);
 
             for (int k = 1; k <= ny; k++) {
                 ANS[k][j] = S[k];
@@ -85,10 +168,22 @@ public:
     }
 
 private:
+    void init_lm() {
+        lm_y.resize(ny+1);
+        for (int k = 1; k <= ny; k++) {
+            lm_y[k] = 4./dy2*asp::sq(sin(k*M_PI*0.5/(ny+1)));
+        }
+        lm_x_.resize(nx+1);
+        for (int j = 1; j <= ny; j++) {
+            lm_x_[j] = 4./dx2*asp::sq(sin(j*M_PI*0.5/(nx+1)));
+        }
+        lm_x = nx == ny ? &lm_y[0] : &lm_x_[0];
+    }
+
     void init_Mat(int i) {
         int di, li, ui;
         di = li = ui = 0;
-        double lm = 4./dy2*asp::sq(sin(i*M_PI*0.5/(ny+1)));
+        double lm = lm_y[i];
         for (int j = 1; j <= nx; j++) {
             D[di++] = -2/dx2-lm;
             if (j > 1) {
--- ut/ut_lapl2d.cpp
+++ ut/ut_lapl2d.cpp
@@ -55,7 +55,7 @@ void test_lapl2d(void** data) {
     double y1 = c->get("test", "y1", 0.0);
     double x2 = c->get("test", "x2", 1.0);
     double y2 = c->get("test", "y2", 1.0);
-    int nx = c->get("test", "nx", 32);
+    int nx = c->get("test", "nx", 31);
     int ny = c->get("test", "ny", 31);
     int verbose = c->get("test", "verbose", 0);
 
@@ -194,13 +194,16 @@ template<typename T>
 void test_lapl2d_norm_decr(void** data) {
     Config* c = static_cast<Config*>(*data);
 
-    int nx = c->get("test", "nx", 16);
+    int nx = c->get("test", "nx", 15);
     int ny = c->get("test", "ny", 15);
 
-    T nrm1 = solve_lapl<T>(c, nx, ny);
-    T nrm2 = solve_lapl<T>(c, nx*2, (ny+1)*2-1);
-
-    assert_true(nrm1/nrm2 > 3.8);
+    double nrm1 = solve_lapl<T>(c, nx, ny);
+    double nrm2 = solve_lapl<T>(c, (nx+1)*2-1, (ny+1)*2-1);
+    int verbose = c->get("test", "verbose", 0);
+    if (verbose) {
+        printf("nrm1/nrm2 = %e, %e %e\n", nrm1/nrm2, nrm1, nrm2);
+    }
+    assert_true(nrm1/nrm2 > 3.7);
 }
 
 void test_lapl2d_norm_decr_double(void** data) {
@@ -221,7 +224,7 @@ void test_lapl2d_ex(void** data) {
     double y1 = c->get("test", "y1", 0.0);
     double x2 = c->get("test", "x2", 1.0);
     double y2 = c->get("test", "y2", 1.0);
-    int nx = c->get("test", "nx", 32);
+    int nx = c->get("test", "nx", 31);
     int ny = c->get("test", "ny", 31);
     int verbose = c->get("test", "verbose", 0);
 
@@ -295,6 +298,79 @@ void test_lapl2d_ex_float(void** data) {
     test_lapl2d_ex<float>(data);
 }
 
+void test_lapl2d_fft1_fft2_cmp(void** data) {
+    using T = double;
+    Config* c = static_cast<Config*>(*data);
+
+    constexpr bool check = true;
+    using matrix = tensor<T,2,check>;
+
+    double x1 = c->get("test", "x1", 0.0);
+    double y1 = c->get("test", "y1", 0.0);
+    double x2 = c->get("test", "x2", 1.0);
+    double y2 = c->get("test", "y2", 1.0);
+    int nx = c->get("test", "nx", 511);
+    int ny = c->get("test", "ny", 511);
+
+    int verbose = c->get("test", "verbose", 0);
+
+    double dx = (x2-x1)/nx, dy = (y2-y1)/ny;
+
+    Lapl2d<T,check> lapl(dx, dy, x2-x1+dx, y2-y1+dy, nx, ny);
+
+    vector<int> indices = {1,ny,1,nx};
+    matrix RHS(indices);
+    matrix ANS(indices);
+    matrix ANS2(indices);
+
+    for (int k = 1; k <= ny; k++) {
+        for (int j = 1; j <= nx; j++) {
+            RHS[k][j] = rp(k, j, dy, dx, x1, y1, x2, y2);
+
+            if (k <= 1) {
+                RHS[k][j] -= ans(k-1, j, dy, dx, x1, y1, x2, y2)/dy/dy;
+            }
+            if (j <= 1) {
+                RHS[k][j] -= ans(k, j-1, dy, dx, x1, y1, x2, y2)/dx/dx;
+            }
+            if (j >= nx) {
+                RHS[k][j] -= ans(k, j+1, dy, dx, x1, y1, x2, y2)/dx/dx;
+            }
+            if (k >= ny) {
+                RHS[k][j] -= ans(k+1, j, dy, dx, x1, y1, x2, y2)/dy/dy;
+            }
+        }
+    }
+
+    {
+        auto t1 = steady_clock::now();
+        lapl.solve1(&ANS[1][1], &RHS[1][1]);
+        auto t2 = steady_clock::now();
+
+        auto interval1 = duration_cast<duration<double>>(t2 - t1).count();
+        if (verbose) {
+            printf("%e\n", interval1);
+        }
+    }
+
+    {
+        auto t1 = steady_clock::now();
+        lapl.solve2(&ANS2[1][1], &RHS[1][1]);
+        auto t2 = steady_clock::now();
+
+        auto interval1 = duration_cast<duration<double>>(t2 - t1).count();
+        if (verbose) {
+            printf("%e\n", interval1);
+        }
+
+        for (int k = 1; k <= ny; k++) {
+            for (int j = 1; j <= nx; j++) {
+                assert_float_equal(ANS[k][j], ANS2[k][j], 1e-15);
+            }
+        }
+    }
+}
+
 int main(int argc, char** argv) {
     string config_fn = "ut_lapl2d.ini";
     Config c;
@@ -308,6 +384,7 @@ int main(int argc, char** argv) {
         cmocka_unit_test_prestate(test_lapl2d_norm_decr_float, &c),
         cmocka_unit_test_prestate(test_lapl2d_ex_double, &c),
         cmocka_unit_test_prestate(test_lapl2d_ex_float, &c),
+        cmocka_unit_test_prestate(test_lapl2d_fft1_fft2_cmp, &c),
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
