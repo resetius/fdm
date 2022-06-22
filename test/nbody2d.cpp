2c248d6a03636bcd0fc2de22a9aeb758dff05764
Alexey Ozeritskiy <aozeritsky@gmail.com>
2022-06-22 15:13:47 +0300

    Ppm method

--- test/nbody2d.cpp
+++ test/nbody2d.cpp
@@ -25,16 +25,28 @@ public:
     double G;
     double vel;
     int sgn;
+    double rcrit;
 
     struct Body {
         T x[2];
         T v[2];
         T mass;
+
+        int k;
+        int j;
+
+        T F[2]; // Local force
+    };
+
+    struct Cell {
+        vector<int> bodies;
+        vector<int> next;
     };
 
     vector<Body> bodies;
     int n0,n1,nn,nnn;
     tensor psi, rhs, f;
+    fdm::tensor<Cell,2,check,flags> cells;
 
     LaplRectFFT2<T,check,flags> solver;
 
@@ -48,6 +60,7 @@ public:
         , G(G)
         , vel(vel)
         , sgn(sgn)
+        , rcrit(h/4)
         , bodies(N)
         , n0(0)
         , n1(flag==tensor_flag::periodic?0:1)
@@ -56,6 +69,7 @@ public:
         , psi({n1,nn,n1,nn})
         , rhs({n1,nn,n1,nn})
         , f({n0,nnn,n0,nnn})
+        , cells({n0,nn,n0,nn})
         , solver(h,h,l,l,n,n)
     {
         init_points();
@@ -100,46 +114,81 @@ public:
 #pragma omp parallel for
         for (int k = n1; k <= nn; k++) {
             for (int j = n1; j <= nn; j++) {
-                rhs[k][j] = sgn*4.*G*M_PI*f[k][j];
+                rhs[k][j] = sgn* 4.*G*M_PI*f[k][j];
             }
         }
         solver.solve(&psi[1][1], &rhs[1][1]);
 
 #pragma omp parallel for
+        for (int k = n0; k <= nn; k++) {
+            for (int j = n0; j <= nn; j++) {
+                cells[k][j].next.clear();
+            }
+        }
+
+#pragma omp parallel for
         for (auto& body : bodies) {
-            int k,j;
+            for (int i = 0; i < 2; i++) {
+                body.F[i] = 0;
+            }
+        }
 
-            double x = body.x[0]-origin[0];
-            double y = body.x[1]-origin[1];
+#pragma omp parallel for
+        for (int k = n0; k <= nn; k++) {
+            for (int j = n0; j <= nn; j++) {
+                auto& cell = cells[k][j];
 
-            if (x < 0 || x > l) continue;
-            if (y < 0 || y > l) continue;
+                // local forces
+                calc_local_forces(cell);
 
-            j = floor(x / h);
-            k = floor(y / h);
+                for (int k0 = -1; k0 <= 1; k0++) {
+                    for (int j0 = -1; j0 <= 1; j0++) {
+                        if (k0 == 0 && j0 == 0) continue;
 
-            if constexpr(flag == tensor_flag::none) {
-                if (k == 0 || j == 0 || k == n || j == n) {
-                    continue;
+                        if constexpr(flag == tensor_flag::periodic) {
+                            calc_local_forces(cell, cells[k+k0][j+j0]);
+                        } else {
+                            if (k+k0 >= 0 && k+k0 <= nn && j+j0 >=0 && j+j0 <= nn) {
+                                calc_local_forces(cell, cells[k+k0][j+j0]);
+                            }
+                        }
+                    }
                 }
             }
+        }
 
-            double a[2];
-            a[0] = (psi[k][j+1]-psi[k][j-1])/2/h;
-            a[1] = (psi[k+1][j]-psi[k-1][j])/2/h;
+#pragma omp parallel for
+        for (int k = n0; k <= nn; k += 2) {
+            for (int j = n0; j <= nn; j += 2) {
+                calc_cell(cells[k][j]);
+            }
+        }
 
-            for (int m = 0; m < 2; m++) {
-                body.v[m] += dt * a[m];
-                body.x[m] += dt * body.v[m];
+#pragma omp parallel for
+        for (int k = n0+1; k <= nn; k += 2) {
+            for (int j = n0; j <= nn; j += 2) {
+                calc_cell(cells[k][j]);
+            }
+        }
 
-                if constexpr(flag == tensor_flag::periodic) {
-                    if (body.x[m] < origin[m]) {
-                        body.x[m] += l;
-                    }
-                    if (body.x[m] >= origin[m]+l) {
-                        body.x[m] -= l;
-                    }
-                }
+#pragma omp parallel for
+        for (int k = n0; k <= nn; k += 2) {
+            for (int j = n0+1; j <= nn; j += 2) {
+                calc_cell(cells[k][j]);
+            }
+        }
+
+#pragma omp parallel for
+        for (int k = n0+1; k <= nn; k += 2) {
+            for (int j = n0+1; j <= nn; j += 2) {
+                calc_cell(cells[k][j]);
+            }
+        }
+
+#pragma omp parallel for
+        for (int k = n0; k <= nn; k++) {
+            for (int j = n0; j <= nn; j++) {
+                cells[k][j].next.swap(cells[k][j].bodies);
             }
         }
 
@@ -163,11 +212,99 @@ public:
         plotter.plot(matrix_plotter::page()
                      .scalar(psi)
                      .levels(10)
-                     .tlabel("Psi")
-                     .bounds(origin[0], origin[1], origin[0]+l, origin[1]+l));
+                     .tlabel("Psi")                     .bounds(origin[0], origin[1], origin[0]+l, origin[1]+l));
     }
 
 private:
+    void apply_bi_bj(Body& bi, Body& bj, bool apply_bj) {
+        double eps = 0.01;
+        double R = 0;
+        for (int k = 0; k < 2; k++) {
+            R += sq(bi.x[k]-bj.x[k]);
+        }
+        R = std::sqrt(R)+eps;
+
+        if (R < rcrit) {
+            for (int k = 0; k < 2; k++) {
+                bi.F[k] += sgn * bj.mass * G * (bi.x[k] - bj.x[k]) /R/R/R;
+            }
+
+            if (apply_bj) {
+                for (int k = 0; k < 2; k++) {
+                    bj.F[k] += sgn * bi.mass * G * (bj.x[k] - bi.x[k]) /R/R/R;
+                }
+            }
+        }
+    }
+
+    void calc_local_forces(Cell& cell) {
+        int nbodies = static_cast<int>(cell.bodies.size());
+
+        for (int i = 0; i < nbodies; i++) {
+            for (int j = i+1; j < nbodies; j++) {
+                auto& bi = bodies[cell.bodies[i]];
+                auto& bj = bodies[cell.bodies[j]];
+
+                apply_bi_bj(bi, bj, true);
+            }
+        }
+    }
+
+    void calc_local_forces(Cell& cell, Cell& other) {
+        for (auto& bi : cell.bodies) {
+            for (auto& bj : other.bodies) {
+                apply_bi_bj(bodies[bi], bodies[bj], false);
+            }
+        }
+    }
+
+    void calc_cell(Cell& cell) {
+        for (auto& index : cell.bodies) {
+            auto& body = bodies[index];
+            if constexpr(flag == tensor_flag::none) {
+                if (body.k == 0 || body.j == 0 || body.k == n+1 || body.j == n+1) {
+                    continue;
+                }
+            }
+
+            int k = body.k; int j = body.j;
+            double a[2];
+
+            // TODO: better interpolate force
+            a[0] = (psi[k][j+1]-psi[k][j-1])/2/h + body.F[0];
+            a[1] = (psi[k+1][j]-psi[k-1][j])/2/h + body.F[1];
+
+            for (int m = 0; m < 2; m++) {
+                body.v[m] += dt * a[m];
+                body.x[m] += dt * body.v[m];
+
+                if constexpr(flag == tensor_flag::periodic) {
+                    if (body.x[m] < origin[m]) {
+                        body.x[m] += l;
+                    }
+                    if (body.x[m] >= origin[m]+l) {
+                        body.x[m] -= l;
+                    }
+                }
+            }
+
+            double x = body.x[0]-origin[0];
+            double y = body.x[1]-origin[1];
+
+            if (x < 0 || x > l) continue;
+            if (y < 0 || y > l) continue;
+
+            int next_j = floor(x / h);
+            int next_k = floor(y / h);
+
+            //verify(abs(body.k-next_k) <= 1, "Too fast, try decrease dt");
+            //verify(abs(body.j-next_j) <= 1, "Too fast, try decrease dt");
+            body.k = next_k; body.j = next_j;
+
+            cells[body.k][body.j].next.push_back(index);
+        }
+    }
+
     void init_points() {
         std::default_random_engine generator;
         std::uniform_real_distribution<T> distribution(0, l/2);
@@ -177,11 +314,16 @@ private:
         T miny = 10000;
         T maxy = -10000;
 
-        for (auto& body : bodies) {
+        for (int index = 0; index < static_cast<int>(bodies.size()); index++) {
+            auto& body = bodies[index];
             for (int i = 0; i < 2; i++) {
                 body.x[i] = distribution(generator) + origin[i] + l/4;
             }
             body.mass = 0.2 + 1.5 * distribution(generator) / l;
+
+            body.j = floor((body.x[0]-origin[0]) / h);
+            body.k = floor((body.x[1]-origin[1]) / h);
+            cells[body.k][body.j].bodies.push_back(index);
 
             minx = min(minx, body.x[0]);
             maxx = max(maxx, body.x[0]);
@@ -222,7 +364,7 @@ void calc(const Config& c) {
         task.step();
 
         if ((step+1) % interval == 0) {
-            task.plot(step);
+            task.plot(step+1);
         }
     }
 }
