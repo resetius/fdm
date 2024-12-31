08a4460a2b2733bad9dd65bb5d8b824b4c4a14df
Alexey Ozeritskiy <aozeritsky@gmail.com>
2024-12-31 16:55:50 +0300

    Add cyclic reduction and test

---
+++ src/cyclic_reduction.h
@@ -0,0 +1,49 @@
+#pragma once
+
+namespace fdm {
+
+// Parallel Solution of Tridiagonal Linear Systems on Modern CPUs
+// Tim Hennes Wichelmann
+// https://www.math.uni-kiel.de/scicom/de/absolventinnen-und-absolventen/tim-wichelmann-bachelorarbeit
+
+template<typename T>
+void cyclic_reduction(
+    T *d, T *e, T *f, T *b,
+    int q, int n)
+{
+    T alpha, gamma;
+    int l, j, s, h;
+
+    e = e-1; // e indices start from 1
+    for (l = 1; l < q; l++) {
+        s = 1 << l;
+        h = 1 << (l-1);
+
+        for (j = s-1; j < n-h; j += s) {
+            alpha = - e[j] / d[j-h];
+            gamma = - f[j] / d[j+h];
+            d[j] += alpha * f[j-h] + gamma * e[j+h];
+            b[j] += alpha * b[j-h] + gamma * b[j+h];
+            e[j] = alpha * e[j-h];
+            f[j] = gamma * f[j+h];
+        }
+    }
+
+    j = (1<<(q-1)) - 1;
+    b[j] = b[j] / d[j];
+
+    for (l = q-1; l > 0; l--) {
+        s = 1 << l;
+        h = 1 << (l-1);
+        j = h-1;
+
+        b[j] = (b[j] - f[j] * b[j+h]) / d[j];
+        for (j = h + s - 1; j < n - h; j += s) {
+            b[j] = (b[j] - e[j] * b[j-h] - f[j] * b[j+h]) / d[j];
+        }
+        b[j] = (b[j] - e[j] * b[j-h]) / d[j];
+    }
+    return;
+}
+
+} // namespace fdm
--- ut/ut_tdiag.cpp
+++ ut/ut_tdiag.cpp
@@ -11,6 +11,7 @@
 #include "asp_gauss.h"
 #include "blas.h"
 #include "config.h"
+#include "cyclic_reduction.h"
 
 extern "C" {
 #include <cmocka.h>
@@ -97,6 +98,83 @@ void test_gtsv_float(void** s) {
     test_gtsv<float>(s);
 }
 
+template<typename T>
+void test_cr(void** data) {
+    Config* c = static_cast<Config*>(*data);
+    int N = c->get("test", "N", 31);
+    int verbose = c->get("test", "verbose", 0);
+    vector<T> A1(N+1);
+    vector<T> A2(N);
+    vector<T> A3(N);
+    vector<T> B(N);
+    vector<T> B1(N);
+    vector<T> B2(N);
+
+    std::default_random_engine generator;
+    std::uniform_real_distribution<T> distribution(-1, 1);
+
+    for (int i = 0; i < N; i++) {
+        A1[i] = distribution(generator);
+        A2[i] = distribution(generator);
+        A3[i] = distribution(generator);
+        B[i] = distribution(generator);
+    }
+
+    {
+        auto A11 = A1;
+        auto A21 = A2;
+        auto A31 = A3;
+        B1 = B;
+
+        auto t1 = steady_clock::now();
+        if constexpr(std::is_same_v<T, double>) {
+            solve_tdiag_linear_my(B1.data(), A11.data(), A21.data(), A31.data(), N);
+        } else {
+            solve_tdiag_linearf_my(B1.data(), A11.data(), A21.data(), A31.data(), N);
+        }
+        auto t2 = steady_clock::now();
+        auto interval = duration_cast<duration<double>>(t2 - t1);
+        if (verbose) {
+            printf("t1=%f\n", interval.count());
+        }
+    }
+
+    {
+        auto A11 = A1;
+        auto A21 = A2;
+        auto A31 = A3;
+        B2 = B;
+        auto t1 = steady_clock::now();
+        int q = ceil(log2(N+1));
+        cyclic_reduction(A21.data(), A11.data(), A31.data(), B2.data(), q, N);
+        auto t2 = steady_clock::now();
+        auto interval = duration_cast<duration<double>>(t2 - t1);
+        if (verbose) {
+            printf("t1=%f\n", interval.count());
+        }
+    }
+
+    double tol = 1e-15;
+    if constexpr(is_same<float,T>::value) {
+        tol = 1e-3;
+    }
+
+    for (int i = 0; i < N; i++) {
+        assert_float_equal(B1[i], B2[i], tol);
+        if (verbose > 1) {
+            printf("%e <> %e\n", B1[i], B2[i]);
+        }
+    }
+}
+
+void test_cr_double(void** s) {
+    test_cr<double>(s);
+}
+
+void test_cr_float(void** s) {
+    test_cr<float>(s);
+}
+
 int main(int argc, char** argv) {
     string config_fn = "ut_tdiag.ini";
     Config c;
@@ -106,8 +184,8 @@ int main(int argc, char** argv) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test_prestate(test_gtsv_float, &c),
         cmocka_unit_test_prestate(test_gtsv_double, &c),
-//        cmocka_unit_test_prestate(test_cr_float, &c),
-//        cmocka_unit_test_prestate(test_cr_double, &c),
+        cmocka_unit_test_prestate(test_cr_float, &c),
+        cmocka_unit_test_prestate(test_cr_double, &c),
     };
 
     return cmocka_run_group_tests(tests, nullptr, nullptr);
