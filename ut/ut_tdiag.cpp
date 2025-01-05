1baec0fb2fb3598e7997aa8b8b4b6f54c137dfcc
Alexey Ozeritskiy <aozeritsky@gmail.com>
2025-01-05 19:55:06 +0000

    Add test

--- ut/ut_tdiag.cpp
+++ ut/ut_tdiag.cpp
@@ -335,6 +335,84 @@ void test_crg_float(void** s) {
     test_crg<float>(s);
 }
 
+// cyclic_reduction_kershaw
+template<typename T>
+void test_crk(void** data) {
+    Config* c = static_cast<Config*>(*data);
+    int N = c->get("test", "N", 32);
+    int verbose = c->get("test", "verbose", 0);
+    vector<T> A1(2*N+1);
+    vector<T> A2(2*N);
+    vector<T> A3(2*N);
+    vector<T> B(2*N);
+    vector<T> B1(2*N);
+    vector<T> B2(2*N);
+
+    std::default_random_engine generator;
+    std::uniform_real_distribution<T> distribution(-1, 1);
+
+    for (int i = 0; i < N; i++) {
+        A1[i] = distribution(generator);
+        A2[i] = distribution(generator);
+        A3[i] = distribution(generator);
+        B[i] = distribution(generator);
+    }
+
+    {
+        auto A11 = A1;
+        auto A21 = A2;
+        auto A31 = A3;
+        B1 = B;
+
+        auto t1 = steady_clock::now();
+        if constexpr(std::is_same_v<T, double>) {
+            solve_tdiag_linear_my(B1.data(), A11.data(), A21.data(), A31.data(), N);
+        } else {
+            solve_tdiag_linearf_my(B1.data(), A11.data(), A21.data(), A31.data(), N);
+        }
+        auto t2 = steady_clock::now();
+        auto interval = duration_cast<duration<double>>(t2 - t1);
+        if (verbose) {
+            printf("t1=%f\n", interval.count());
+        }
+    }
+
+    {
+        auto A11 = A1;
+        auto A21 = A2;
+        auto A31 = A3;
+        B2 = B;
+        auto t1 = steady_clock::now();
+        int q = ceil(log2(N+1));
+        cyclic_reduction_kershaw(A21.data(), A11.data(), A31.data(), B2.data(), q, N);
+        auto t2 = steady_clock::now();
+        auto interval = duration_cast<duration<double>>(t2 - t1);
+        if (verbose) {
+            printf("t1=%f\n", interval.count());
+        }
+    }
+
+    double tol = 1e-15;
+    if constexpr(is_same<float,T>::value) {
+        tol = 1e-3;
+    }
+
+    for (int i = 0; i < N; i++) {
+        assert_float_equal(B1[i], B2[i], tol);
+        if (verbose > 1) {
+            printf("%e <> %e\n", B1[i], B2[i]);
+        }
+    }
+}
+
+void test_crk_double(void** s) {
+    test_crk<double>(s);
+}
+
+void test_crk_float(void** s) {
+    test_crk<float>(s);
+}
+
 int main(int argc, char** argv) {
     string config_fn = "ut_tdiag.ini";
     Config c;
@@ -350,6 +428,8 @@ int main(int argc, char** argv) {
         cmocka_unit_test_prestate(test_cr_double, &c),
         cmocka_unit_test_prestate(test_crg_float, &c),
         cmocka_unit_test_prestate(test_crg_double, &c),
+        cmocka_unit_test_prestate(test_crk_float, &c),
+        cmocka_unit_test_prestate(test_crk_double, &c),
     };
 
     return cmocka_run_group_tests(tests, nullptr, nullptr);
