ec084fbed1601c2745f04a87216dd11624fddef5
Alexey Ozeritskiy <aozeritsky@gmail.com>
2025-03-15 22:26:15 +0000

    Reformat code

--- src/big_float.h
+++ src/big_float.h
@@ -824,31 +824,23 @@ private:
 
         if (bitShift > 0) {
             BlockType mask = (1ULL << bitShift) - 1ULL;
-            if constexpr(std::is_same_v<BlockType,uint64_t>) {
+            int i;
+            for (i = 0; i < blocks - blockShift - 1; i++) {
+                carry = mantissa[i+1] & mask;
+                if constexpr(std::is_same_v<BlockType,uint64_t>) {
 #ifdef __x86_64__
-                int i;
-                for (i = 0; i < blocks - blockShift - 1; i++) {
-                    carry = mantissa[i+1] & mask;
                     asm volatile ("shrd %2, %1, %0"
                         : "+r" (mantissa[i])
                         : "r" (carry), "c" ((unsigned char)bitShift)
                     );
-                }
-                mantissa[i] >>= bitShift;
 #else
-                for (int i = blocks - blockShift - 1; i >= 0; --i) {
-                    BlockType next_carry = mantissa[i] & mask;
                     mantissa[i] = (mantissa[i] >> bitShift) | (carry << (blockBits - bitShift));
-                    carry = next_carry;
-                }
 #endif
-            } else {
-                for (int i = blocks - blockShift - 1; i >= 0; --i) {
-                    BlockType next_carry = mantissa[i] & mask;
+                } else {
                     mantissa[i] = (mantissa[i] >> bitShift) | (carry << (blockBits - bitShift));
-                    carry = next_carry;
                 }
             }
+            mantissa[i] >>= bitShift;
         }
     }
 
