0358b109f900f420137921b10d415a65502856f0
Alexey Ozeritskiy <aozeritsky@gmail.com>
2025-06-18 23:37:29 +0100

    Fix unixbench score calculation

--- src/unixbench_score.cpp
+++ src/unixbench_score.cpp
@@ -5,9 +5,10 @@
 
 namespace fdm {
 
+// https://www.alibabacloud.com/blog/unixbench-score-an-introduction_594677
 double unixbench_score(std::vector<double>& data) {
     if (data.empty()) return 0.0;
-    std::sort(data.begin(), data.end(), std::greater<double>());
+    std::sort(data.begin(), data.end(), std::less<double>());
     int keep = (2 * data.size()) / 3;
     data.resize(keep);
     double score = 0.0;
